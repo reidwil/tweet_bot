fef3bf35bd0a9b472141dab89a509df3568ac2e0
reidwil <44632147+reidwil@users.noreply.github.com>
2020-03-15 21:46:18 -0600

    Update main.R

--- main.R
+++ main.R
@@ -3,12 +3,15 @@ library(dplyr)
 library(rtweet)
 
 ## call my token script to interact with twitter
-source("env.r")
+source("~/R/tweetr/env.r")
 
 ## get my feed
 feed <- get_my_timeline(n = 1e10)
 
-## clean data to only get media related tweets
+## clean data to get 
+## 1. media related tweets with 1k+ favorites
+## 2. text related tweets with 10k+ favorites
+
 funny_photos <- 
   feed %>% 
     filter(media_type == 'photo') %>% 
@@ -16,7 +19,16 @@ funny_photos <-
     mutate(status_id = as.character(status_id)) %>% 
     select(status_id)
 
-## retweet the funny pictures!
-for(i in 1:nrow(funny_photos)) {
-  post_tweet(retweet_id = funny_photos$status_id[[i]])
+funny_tweets <-
+  feed %>% 
+    filter(is.na(media_type)) %>% 
+    filter(favorite_count > 9999) %>% 
+    mutate(status_id = as.character(status_id)) %>% 
+    select(status_id)
+
+tweets <- rbind(funny_photos, funny_tweets)
+
+## retweet the funny stuff!
+for(i in 1:nrow(tweets)) {
+  post_tweet(retweet_id = tweets$status_id[[i]])
 }
