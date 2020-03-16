6ac26f9bf0d46c5c129543a8c350f5c025a1f866
reidwil <44632147+reidwil@users.noreply.github.com>
2020-03-15 23:13:33 -0600

    Update env.r

--- env.r
+++ env.r
@@ -1,15 +1,20 @@
 # Secrets
-consumer_key <- Sys.getenv("CONSUMER_KEY")
+consumer_key    <- Sys.getenv("CONSUMER_KEY")
 consumer_secret <- Sys.getenv("CONSUMER_SECRET")
-access_token <- Sys.getenv("ACCESS_TOKEN")
-access_secret <- Sys.getenv("ACCESS_SECRET")
+access_token    <- Sys.getenv("ACCESS_TOKEN")
+access_secret   <- Sys.getenv("ACCESS_SECRET")
 
 token <- create_token(
-  app = "reids_tweetr",
-  consumer_key = consumer_key,
+  app             = "reids_tweetr",
+  consumer_key    = consumer_key,
   consumer_secret = consumer_secret, 
-  access_token = access_token, 
-  access_secret = access_secret
+  access_token    = access_token, 
+  access_secret   = access_secret
 )
 
-rm(consumer_key, consumer_secret, access_token, access_secret)
+rm(
+  consumer_key, 
+  consumer_secret, 
+  access_token, 
+  access_secret
+)
